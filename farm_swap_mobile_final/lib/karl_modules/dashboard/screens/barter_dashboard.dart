44624d5c4826a75b6d9e7b78f8fc04f8d3744430
EternalRex <karlevemarx@gmail.com>
2023-11-09 20:46:28 +0800

    mag display sa dashboard sa products to barter

--- farm_swap_mobile_final/lib/karl_modules/dashboard/screens/barter_dashboard.dart
+++ farm_swap_mobile_final/lib/karl_modules/dashboard/screens/barter_dashboard.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter_screenutil/flutter_screenutil.dart";
 
 class BarterDashboard extends StatefulWidget {
   const BarterDashboard({super.key});
@@ -10,12 +11,55 @@ class BarterDashboard extends StatefulWidget {
 class _BarterDashboardState extends State<BarterDashboard> {
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
-        SizedBox(
-          child: Text("Hi"),
-        )
+        Expanded(
+          child: SingleChildScrollView(
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+              ),
+              height: 480.h,
+              child: TwoItemsListView(),
+            ),
+          ),
+        ),
       ],
     );
   }
+}
+
+class TwoItemsListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: yourDataList.length, // Replace with your data list length
+      itemBuilder: (context, index) {
+        // Check if the current index is even or odd to display two items in a row.
+        if (index.isEven) {
+          return Row(
+            children: <Widget>[
+              Expanded(
+                child: yourItemWidget(index),
+              ),
+              Expanded(
+                child: yourItemWidget(index + 1),
+              ),
+            ],
+          );
+        }
+        return Divider(); // You can use a divider or separator between rows.
+      },
+    );
+  }
+
+  Widget yourItemWidget(int index) {
+    // Create your item widget based on the index.
+    return ListTile(
+      title: Text('Item $index'),
+      // Customize this widget based on your data.
+    );
+  }
+
+  List<String> yourDataList = List.generate(20, (index) => 'Item $index');
 }
